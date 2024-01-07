36db7b33f0195c3474322731c3ec8f0dd0877e13
Rizky905 <erlangga905@gmail.com>
2024-01-08 00:38:57 +0700

    first commit

---
+++ prisma/migrations/20240107172258_create_tables_user_and_contacts/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `username` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `token` VARCHAR(100) NULL,
+
+    PRIMARY KEY (`username`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `contacts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(100) NOT NULL,
+    `last_name` VARCHAR(100) NULL,
+    `email` VARCHAR(200) NULL,
+    `phone` VARCHAR(20) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ readme.md
@@ -0,0 +1,43 @@
+## This development using TypeScript
+
+- npm init -y
+- npm i -D typescript @types/express @types/node
+- npx tsc --init
+
+## Joi
+
+for validation input
+
+## Prisma
+
+for database orm
+
+- migrate :
+  -- for the fixed or default name migrate example : create_table_users
+
+  -- npx prisma migrate dev --create-only
+  -- npx prisma migrate dev
+
+## Winston
+
+for logger
+
+## Bcrypt
+
+for hash
+
+## UUID
+
+For generate uuid
+
+## Jest
+
+libarary jest for Unit test
+
+## Babel-jest
+
+for read Jest to js type module
+
+## Supertest
+
+for testing
